0a547be7570c702e602fbdb6dedfecaaec97314c
Ilia <terpariz@mail.ru>
2019-10-05 18:40:23 +0300

    UI hidding when pause button was pressed

--- ChessClock/Clocks/ViewController.swift
+++ ChessClock/Clocks/ViewController.swift
@@ -12,6 +12,12 @@ class ViewController: UIViewController {
 
     @IBOutlet weak var topChrono: UILabel!
     @IBOutlet weak var bottomChrono: UILabel!
+    @IBOutlet weak var bottomClockButton: UIButton!
+    @IBOutlet weak var topClockButton: UIButton!
+    @IBOutlet weak var pauseButton: UIButton!
+    @IBOutlet weak var resetButton: UIButton!
+    @IBOutlet weak var resetButtonTrailingConstraint: NSLayoutConstraint!
+
     var presenter: ClocksPresenter!
 
     override func viewDidLoad() {
@@ -27,9 +33,11 @@ class ViewController: UIViewController {
     }
     @IBAction func PauseButtonPressed(_ sender: Any) {
         presenter.clocksStateBehaviourSubject.onNext(ClocksEvents.pause)
+        neededToHide(true)
     }
     @IBAction func resetButtonPressed(_ sender: Any) {
         presenter.clocksStateBehaviourSubject.onNext(ClocksEvents.restart)
+        neededToHide(false)
     }
 }
 
@@ -44,5 +52,33 @@ private extension ViewController {
 
     private func customize() {
         topChrono.transform = CGAffineTransform(rotationAngle: CGFloat.pi)
+    }
+}
+
+// MARK: - Extension to manage UI showing
+
+extension ViewController {
+    
+    private func neededToHide(_ trigger: Bool) {
+        
+        let viewsToHide = [topChrono, bottomChrono, topClockButton, bottomClockButton, pauseButton]
+        for view in viewsToHide {
+            view?.isHidden = trigger
+        }
+        
+        resetButtonAnimation(trigger)
+    }
+    
+    private func resetButtonAnimation(_ trigger: Bool) {
+        
+        if trigger {
+            resetButtonTrailingConstraint.constant = (view.frame.width / 2) - (resetButton.frame.width / 2)
+        } else {
+            resetButtonTrailingConstraint.constant = 106
+        }
+        
+        UIView.animate(withDuration: 0.3) {
+            self.view.layoutIfNeeded()
+        }
     }
 }
